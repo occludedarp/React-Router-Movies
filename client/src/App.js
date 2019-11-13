a6a18e2ccd8d6d00fe2dd124b5462a4bc2854942
occludedarp <cmrnmr@gmail.com>
2019-11-12 23:04:23 -0500

    Completed one stretch goal

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,46 @@
 import React, { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    //Joscelyns conditional logic for preventing duplicates appearing in saved
+    if(
+      savedList.find((savedMovie)=> {
+        return movie.id === savedMovie.id;
+      }) === undefined
+    ) {
+    //
+      setSavedList( [...savedList, movie] ); //was outside of brackets prior to conditional logic
+    }
+
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route 
+        exact
+        path="/" 
+        render={() => (
+          <MovieList/>
+        )} 
+      />    
+      <Route 
+      
+        path="/Movies/:id" 
+        render={(props) => (
+                          //added as part of stretch feature
+          <Movie {...props} addToSavedList={addToSavedList}/>
+
+        )}
+       />
+          
     </div>
   );
 };
